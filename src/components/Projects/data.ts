f29d8c17b23434273ab6494efaeee4fe7a9c253c
Glutix <ferreyra.gluti@gmail.com>
2023-07-19 07:28:53 -0300

    fix/styles

--- src/components/Projects/data.ts
+++ src/components/Projects/data.ts
@@ -2,6 +2,7 @@ import { Project } from "../../utils/interfaces";
 import facilMarket from "../../assets/Projects/FacilMarket.png";
 import videoGame from "../../assets/Projects/VideoGame.png";
 import rickAndMorty from "../../assets/Projects/Rick&Morty.png";
+import portfolio from "../../assets/Projects/portfolio.png";
 
 const data: Project = [
 	{
@@ -9,7 +10,7 @@ const data: Project = [
 		image: { link: `${facilMarket}`, name: "Projecto-image: Facil Market" },
 		repository: "https://github.com/Glutix/facil-market-proyect",
 		description:
-			"Facil Market es una aplicación web robusta y versátil que brinda a los usuarios la posibilidad de realizar compras y ventas de productos, además de facilitar la conexión entre ellos.",
+			"Facil Market es una aplicación web robusta y versátil que brinda a los usuarios la posibilidad de realizar compras y ventas de productos.",
 		demo: "https://www.youtube.com/watch?v=XQNHgjvIK84",
 	},
 	{
@@ -17,7 +18,16 @@ const data: Project = [
 		image: { link: `${videoGame}`, name: "Projecto-image: Video Games" },
 		repository: "https://github.com/Glutix/VideoGames",
 		description:
-			"Aplicación web de videojuegos con fines académicos, cuenta con características como filtros combinados, ordenamiento, paginación y búsqueda por nombre, entre otras.",
+			"Aplicación de videojuegos con fines académicos, cuenta con características como filtros combinados, ordenamiento, paginación y búsquedas, etc.",
+		demo: "string",
+	},
+
+	{
+		name: "Portfolio",
+		image: { link: `${portfolio}`, name: "Projecto-image: Portfolio" },
+		repository: "https://github.com/Glutix/Portfolio",
+		description:
+			"¡Este es mi primer portfolio creado como carta de presentación y con fines educativos!",
 		demo: "string",
 	},
 
@@ -26,7 +36,7 @@ const data: Project = [
 		image: { link: `${rickAndMorty}`, name: "Projecto-image: Rick and Morty" },
 		repository: "https://github.com/Glutix/Rick_and_Morty",
 		description:
-			"Aplicación web con fines académicos donde cuenta con las siguientes funcionalidades, agregar cartas de personajes ingresando un id, filtros, ordenamiento, favoritos, etc...",
+			"Aplicación con fines académicos donde cuenta con las siguientes funcionalidades, añadir cartas, filtros, ordenamiento, favoritos, etc.",
 		demo: "string",
 	},
 ];
