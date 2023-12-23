b338cf0f90ca30c9ecad0429eb5975e39a653a0b
Glutix <ferreyra.gluti@gmail.com>
2023-12-23 15:55:45 -0300

    fix/logo-and-image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,73 +1,77 @@
-import gluti from "../assets/Gluti.jpg";
+import glutix from "../assets/asdasdasd.jpg";
 import gitHub from "../assets/icons/github.svg";
 import linkedin from "../assets/icons/linkedin.svg";
 import instagram from "../assets/icons/instagram.svg";
 import twitter from "../assets/icons/twitter.svg";
 
 const About = () => {
-	return (
-		<div className="about">
-			<div className="about-conteiner-card">
-				<div className="box" data-aos="zoom-in-right">
-					<h2>Ferreyra Ricardo</h2>
+  return (
+    <div className="about">
+      <div className="about-conteiner-card">
+        <div className="box" data-aos="zoom-in-right">
+          <h2>Ferreyra Ricardo</h2>
 
-					<img src={gluti} alt="Foto de perfil" className="box-image-profile" />
+          <img
+            src={glutix}
+            alt="Foto de perfil"
+            className="box-image-profile"
+          />
 
-					<h2>Full Stack Developer</h2>
-				</div>
-			</div>
+          <h2>Full Stack Developer</h2>
+        </div>
+      </div>
 
-			<div className="about-conteiner-info" data-aos="fade-left">
-				<section className="info-about">
-					<h2>Sobre mí</h2>
-					<p>
-						¡Hola! 👋 Soy Ricardo, un apasionado desarrollador Full Stack con
-						experiencia en la creación de aplicaciones web innovadoras y
-						escalables. Me especializo en el uso de TypeScript con React y
-						Express para construir soluciones tecnológicas de alto rendimiento.
-					</p>
-				</section>
+      <div className="about-conteiner-info" data-aos="fade-left">
+        <section className="info-about">
+          <h2>Sobre mí</h2>
+          <p>
+            ¡Hola! 👋 Soy Ricardo, un apasionado desarrollador Full Stack con
+            experiencia en la creación de aplicaciones web innovadoras y
+            escalables. Me especializo en el uso de TypeScript con React y
+            Express para construir soluciones tecnológicas de alto rendimiento.
+          </p>
+        </section>
 
-				<a
-					href="https://drive.google.com/file/d/1fDr3h5t0QLQrNfa3_w-lsA5UeRElE_1f/view?usp=sharing"
-					target="_blank"
-					rel="noopener noreferrer"
-					download
-				>
-					<button>Curriculum</button>
-				</a>
+        <a
+          href="https://drive.google.com/file/d/1fDr3h5t0QLQrNfa3_w-lsA5UeRElE_1f/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          download
+        >
+          <button>Curriculum</button>
+        </a>
 
-				<section className="info-redes">
-					<h2>Redes Sociales</h2>
-					<div className="conteiner-icons">
-						<a href="https://github.com/Glutix" target="_blank">
-							<img className="icon" src={gitHub} alt="icon-gitHub" />
-						</a>
+        <section className="info-redes">
+          <h2>Redes Sociales</h2>
+          <div className="conteiner-icons">
+            <a href="https://github.com/Glutix" target="_blank">
+              <img className="icon" src={gitHub} alt="icon-gitHub" />
+            </a>
 
-						<a
-							href="https://www.linkedin.com/in/ricardo-ferreyra/"
-							target="_blank"
-						>
-							<img className="icon" src={linkedin} alt="icon-linkedin" />
-						</a>
+            <a
+              href="https://www.linkedin.com/in/ricardo-ferreyra/"
+              target="_blank"
+            >
+              <img className="icon" src={linkedin} alt="icon-linkedin" />
+            </a>
 
-						<a href="https://twitter.com/Glutix_32" target="_blank">
-							<img className="icon" src={twitter} alt="icon-twitter" />
-						</a>
+            <a href="https://twitter.com/Glutix_32" target="_blank">
+              <img className="icon" src={twitter} alt="icon-twitter" />
+            </a>
 
-						<a href="https://www.instagram.com/glutinose/" target="_blank">
-							<img
-								className="icon"
-								src={instagram}
-								alt="icon-instagram"
-								loading="lazy"
-							/>
-						</a>
-					</div>
-				</section>
-			</div>
-		</div>
-	);
+            <a href="https://www.instagram.com/glutinose/" target="_blank">
+              <img
+                className="icon"
+                src={instagram}
+                alt="icon-instagram"
+                loading="lazy"
+              />
+            </a>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,46 +1,47 @@
 import { useState } from "react";
+import logo from "../assets/logo1.png";
 
 const NavBar = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	document.body.style.overflow = !isOpen ? "auto" : "hidden";
+  const [isOpen, setIsOpen] = useState(false);
+  document.body.style.overflow = !isOpen ? "auto" : "hidden";
 
-	const handleClick = () => {
-		setIsOpen(!isOpen);
-	};
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-	const handleAncor = () => {
-		setIsOpen(false);
-	};
+  const handleAncor = () => {
+    setIsOpen(false);
+  };
 
-	return (
-		<div className="nav-conteiner">
-			<h2 className="logoText">Ferreyra</h2>
+  return (
+    <div className="nav-conteiner">
+      <img className="logo" src={logo} />
 
-			<div className={`nav-section ${isOpen && "nav-section-open"}`}>
-				<a href="#" className="link" onClick={handleAncor}>
-					Inicio
-				</a>
-				<a href="#projects" className="link" onClick={handleAncor}>
-					Proyectos
-				</a>
-				<a href="#skills" className="link" onClick={handleAncor}>
-					Habilidades
-				</a>
-				<a href="#contact" className="link" onClick={handleAncor}>
-					Contacto
-				</a>
-			</div>
+      <div className={`nav-section ${isOpen && "nav-section-open"}`}>
+        <a href="#" className="link" onClick={handleAncor}>
+          Inicio
+        </a>
+        <a href="#projects" className="link" onClick={handleAncor}>
+          Proyectos
+        </a>
+        <a href="#skills" className="link" onClick={handleAncor}>
+          Habilidades
+        </a>
+        <a href="#contact" className="link" onClick={handleAncor}>
+          Contacto
+        </a>
+      </div>
 
-			<div
-				className={`toggle ${isOpen && "toggleSpanOpen"}`}
-				onClick={handleClick}
-			>
-				<span className={`span ${isOpen && "s1"}`}></span>
-				<span className={`span ${isOpen && "s2"}`}></span>
-				<span className={`span ${isOpen && "s3"}`}></span>
-			</div>
-		</div>
-	);
+      <div
+        className={`toggle ${isOpen && "toggleSpanOpen"}`}
+        onClick={handleClick}
+      >
+        <span className={`span ${isOpen && "s1"}`}></span>
+        <span className={`span ${isOpen && "s2"}`}></span>
+        <span className={`span ${isOpen && "s3"}`}></span>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
